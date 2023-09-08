b92cee3cbd936162bd95af04c4dc9e98531b81bd
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-09-07 21:10:31 -0300

    first commit

---
+++ src/app/cadastrar/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const CadastrarNovo = () => {
+    return (
+        <>
+            <p>Página Cadastrar novo</p>
+            <Link href={'/'}>Home</Link>
+        </>
+    )
+}
+
+export default CadastrarNovo
