cf5b06a52233d3213344e6ffb7d69944bab509ed
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-09-13 21:50:48 -0300

    Ajuste botão adicionar veículo

--- src/app/logged-area/veiculos/components/vehicles-header.tsx
+++ src/app/logged-area/veiculos/components/vehicles-header.tsx
@@ -8,7 +8,7 @@ export default function VehiclesLayoutHeader({titulo,}: {titulo: string}) {
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">{titulo}</h1>
           </div>
           <div className="max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex content-center">
-            <AppButton className={'bg-sky-700 text-white rounded p-1 flex space-x-1 p-2 hover:bg-sky-900'}>
+            <AppButton className={'bg-sky-700 text-white rounded-lg shadow p-1 flex space-x-1 p-2 hover:bg-sky-900'}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
